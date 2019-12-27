f0914371a8bab7e8db683dd9142bac42356c07b7
henryyyk <henryyyk@users.noreply.github.com>
2019-12-27 19:40:07 -0300

    correção do carousel ficar por cima do menu

--- _script/scripts.js
+++ _script/scripts.js
@@ -4,6 +4,7 @@ $('document').ready(function(){
         loop: true,
         autoplay: true,
         autoplayTimeout: 3000, // ele só aceita o tem e mili segundos 
+        center: true,
         responsive: {
             0: { // de 0px até 360px ele mostrará apenas 1 item
                 items: 1 
